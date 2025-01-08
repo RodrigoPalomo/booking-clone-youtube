5c20f15d1d44955837a729fc747606702df7774a
Rodrigo Palomo <repmlm@hotmail.com>
2025-01-08 15:30:46 -0300

    Booking Project (I need more images. I'll take a break)

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-[#013B94]' >
+        <nav className='mx-auto flex max-w-7-xl items-center justify-between p-6 lg:px-8' aria-label="Global">
+          
+        </nav>
+    </header>
+  )
+}
+
+export default Header
